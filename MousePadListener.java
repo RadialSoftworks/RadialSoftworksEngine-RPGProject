0b97a12221ed9abad3b5c5a586639a52ca969006
Robert <xaremx@gmail.com>
2014-09-16 19:59:02 -0500

    Another class!

---
+++ MousePadListener.java
@@ -0,0 +1,40 @@
+package com.RadialSoftworks.www;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MousePadListener implements MouseListener {
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}
